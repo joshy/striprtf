a34463f86cef6ca0e350790828d8d490feabaaca
Joshy Cyriac <j.cyriac@gmail.com>
2018-02-27 15:37:42 +0100

    initial commit

---
+++ tests/test_simple.py
@@ -0,0 +1,30 @@
+import unittest
+from pathlib import Path
+from filecmp import cmp
+
+from striprtf.striprtf import rtf_to_text
+
+RTF_DIR = Path.cwd() / 'tests' / 'rtf'
+TEXT_DIR = Path.cwd() / 'tests' / 'text'
+
+class TestSimple(unittest.TestCase):
+
+    def test_empty(self):
+        hello_rtf = RTF_DIR / 'hello.rtf'
+        hello_txt = TEXT_DIR / 'hello.txt'
+
+        with hello_rtf.open() as source:
+            result = rtf_to_text(source.read())
+        with hello_txt.open() as destination:
+            self.assertEqual(destination.read(), result)
+
+    def test_table(self):
+        simple_table_rtf = RTF_DIR / 'simple_table.rtf'
+        simple_table_txt = TEXT_DIR / 'simple_table.txt'
+
+        with simple_table_rtf.open() as source:
+            result = rtf_to_text(source.read())
+        with simple_table_txt.open() as destination:
+            self.assertEqual(destination.read(), result)
+
+

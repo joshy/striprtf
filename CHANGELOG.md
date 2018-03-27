0216ddd73ed615147ab3463e99a917012485db6b
Joshy Cyriac <j.cyriac@gmail.com>
2018-03-27 09:56:27 +0200

    new minor version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
 # Changelog
 
+
+## v0.0.2 - 27.03.2018
+ * nested cells are also separated with '|'
 ## v0.0.1 - 28.02.2018
  * Initial version
--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'striprtf',
   packages = ['striprtf'], # this must be the same as the name above
-  version = '0.0.1',
+  version = '0.0.2',
   description = 'A simple library to convert rtf to text',
   author = 'Joshy Cyriac',
   author_email = 'j.cyriac@gmail.com',
   url = 'https://github.com/joshy/striprtf', # use the URL to the github repo
-  download_url = 'https://github.com/joshy/striprtf/archive/0.1.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/joshy/striprtf/archive/0.0.2.tar.gz',
   keywords = ['rtf'],
   classifiers = [],
 )

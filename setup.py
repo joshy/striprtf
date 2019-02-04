6c3fc6cec0c6c28e8f5a1ca24a76f6746af64186
Joshy Cyriac <j.cyriac@gmail.com>
2019-02-04 22:57:57 +0100

    added cli

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
 from distutils.core import setup
+
+# read the contents of your README file
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+
 setup(
-  name = 'striprtf',
-  packages = ['striprtf'], # this must be the same as the name above
-  version = '0.0.3',
-  description = 'A simple library to convert rtf to text',
-  author = 'Joshy Cyriac',
-  author_email = 'j.cyriac@gmail.com',
-  url = 'https://github.com/joshy/striprtf', # use the URL to the github repo
-  download_url = 'https://github.com/joshy/striprtf/archive/v0.0.3.tar.gz',
-  keywords = ['rtf'],
-  classifiers = [],
+    name="striprtf",
+    packages=["striprtf"],
+    version="0.0.4",
+    description="A simple library to convert rtf to text",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Joshy Cyriac",
+    author_email="j.cyriac@gmail.com",
+    url="https://github.com/joshy/striprtf",
+    download_url="https://github.com/joshy/striprtf/archive/v0.0.3.tar.gz",
+    keywords=["rtf"],
+    scripts=["striprtf/striprtf"],
+    classifiers=[],
 )

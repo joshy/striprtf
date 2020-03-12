d80d2f7c40386436e10f90902092169d0cf06e34
Joshy Cyriac <j.cyriac@gmail.com>
2020-03-12 10:08:29 +0100

    Fix for https://github.com/joshy/striprtf/issues/6

--- striprtf/striprtf.py
+++ striprtf/striprtf.py
@@ -4,6 +4,8 @@ Taken from https://gist.github.com/gilsondev/7c1d2d753ddb522e7bc22511cfb08676
 and modified for better output of tables.
 """
 
+
+# fmt: off
 # control words which specify a "destination".
 destinations = frozenset((
     'aftncn','aftnsep','aftnsepc','annotation','atnauthor','atndate','atnicn','atnid',
@@ -47,72 +49,77 @@ destinations = frozenset((
     'xmlattrname','xmlattrvalue','xmlclose','xmlname','xmlnstbl',
     'xmlopen',
     ))
+# fmt: on
 
 
 # Translation of some special characters.
 specialchars = {
-    'par': '\n',
-    'sect': '\n\n',
-    'page': '\n\n',
-    'line': '\n',
-    'tab': '\t',
-    'emdash': '\u2014',
-    'endash': '\u2013',
-    'emspace': '\u2003',
-    'enspace': '\u2002',
-    'qmspace': '\u2005',
-    'bullet': '\u2022',
-    'lquote': '\u2018',
-    'rquote': '\u2019',
-    'ldblquote': '\201C',
-    'rdblquote': '\u201D',
-    'row':'\n',
-    'cell': '|',
-    'nestcell': '|'
+    "par": "\n",
+    "sect": "\n\n",
+    "page": "\n\n",
+    "line": "\n",
+    "tab": "\t",
+    "emdash": "\u2014",
+    "endash": "\u2013",
+    "emspace": "\u2003",
+    "enspace": "\u2002",
+    "qmspace": "\u2005",
+    "bullet": "\u2022",
+    "lquote": "\u2018",
+    "rquote": "\u2019",
+    "ldblquote": "\201C",
+    "rdblquote": "\u201D",
+    "row": "\n",
+    "cell": "|",
+    "nestcell": "|",
     }
 
-PATTERN  = re.compile(r"\\([a-z]{1,32})(-?\d{1,10})?[ ]?|\\'([0-9a-f]{2})|\\([^a-z])|([{}])|[\r\n]+|(.)", re.I)
+PATTERN = re.compile(
+    r"\\([a-z]{1,32})(-?\d{1,10})?[ ]?|\\'([0-9a-f]{2})|\\([^a-z])|([{}])|[\r\n]+|(.)",
+    re.I,
+)
+
 
 def rtf_to_text(text):
     stack = []
-    ignorable = False       # Whether this group (and all inside it) are "ignorable".
-    ucskip = 1              # Number of ASCII characters to skip after a unicode character.
-    curskip = 0             # Number of ASCII characters left to skip
-    out = []                # Output buffer.
+    ignorable = False  # Whether this group (and all inside it) are "ignorable".
+    ucskip = 1  # Number of ASCII characters to skip after a unicode character.
+    curskip = 0  # Number of ASCII characters left to skip
+    out = []  # Output buffer.
     for match in PATTERN.finditer(text):
-        word,arg,hex,char,brace,tchar = match.groups()
+        word, arg, hex, char, brace, tchar = match.groups()
         if brace:
             curskip = 0
-            if brace == '{':
+            if brace == "{":
                 # Push state
-                stack.append((ucskip,ignorable))
-            elif brace == '}':
+                stack.append((ucskip, ignorable))
+            elif brace == "}":
                 # Pop state
                 try:
-                    ucskip,ignorable = stack.pop()
+                    ucskip, ignorable = stack.pop()
                 # sample_3.rtf throws an IndexError because of stack being empty.
                 # don't know right now how this could happen, so for now this is
                 # a ugly hack to prevent it
                 except IndexError:
                     ucskip = 0
                     ignorable = True
-        elif char: # \x (not a letter)
+        elif char:  # \x (not a letter)
             curskip = 0
-            if char == '~':
+            if char == "~":
                 if not ignorable:
-                    out.append('\xA0') # NBSP
-            elif char in '{}\\':
+                    out.append("\xA0")  # NBSP
+            elif char in "{}\\":
                 if not ignorable:
                     out.append(char)
-            elif char == '*':
+            elif char == "*":
                 ignorable = True
-            elif char == '\n':
+            elif char == "\n":
                 if not ignorable:
-                    out.append('\x0A')  # LF
-            elif char == '\r':
+                    out.append("\x0A")  # LF
+            elif char == "\r":
                 if not ignorable:
-                    out.append('\x0D')  # CR
-        elif word: # \foo
+                    out.append("\x0D")  # CR
+        elif word:  # \foo
             curskip = 0
             if word in destinations:
                 ignorable = True
@@ -120,24 +127,33 @@ def rtf_to_text(text):
                 pass
             elif word in specialchars:
                 out.append(specialchars[word])
-            elif word == 'uc':
+            elif word == "uc":
                 ucskip = int(arg)
-            elif word == 'u':
+            elif word == "u":
+                # because of https://github.com/joshy/striprtf/issues/6
+                if arg is None:
+                    curskip = ucskip
+                else:
                 c = int(arg)
-                if c < 0: c += 0x10000
-                if c > 127: out.append(chr(c)) #NOQA
-                else: out.append(chr(c))
+                    if c < 0:
+                        c += 0x10000
+                    if c > 127:
+                        out.append(chr(c))  # NOQA
+                    else:
+                        out.append(chr(c))
                 curskip = ucskip
-        elif hex: # \'xx
+        elif hex:  # \'xx
             if curskip > 0:
                 curskip -= 1
             elif not ignorable:
-                c = int(hex,16)
-                if c > 127: out.append(chr(c)) #NOQA
-                else: out.append(chr(c))
+                c = int(hex, 16)
+                if c > 127:
+                    out.append(chr(c))  # NOQA
+                else:
+                    out.append(chr(c))
         elif tchar:
             if curskip > 0:
                 curskip -= 1
             elif not ignorable:
                 out.append(tchar)
-    return ''.join(out)
+    return "".join(out)

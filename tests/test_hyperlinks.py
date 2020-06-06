2a749b1b4a91de7b7556b930d3abb9ae9d03bdfa
Joshy Cyriac <j.cyriac@gmail.com>
2020-06-06 17:38:58 +0200

    removed print statements

--- tests/test_hyperlinks.py
+++ tests/test_hyperlinks.py
@@ -8,15 +8,13 @@ RTF_DIR = Path.cwd() / "tests" / "rtf"
 TEXT_DIR = Path.cwd() / "tests" / "text"
 
 
-class TestHello(unittest.TestCase):
+class TestHyperlinks(unittest.TestCase):
     def test_empty(self):
         example_rtf = RTF_DIR / "hyperlinks.rtf"
         example_txt = TEXT_DIR / "hyperlinks.txt"
 
         with example_rtf.open() as source:
             result = rtf_to_text(source.read())
-        print("------------------°°°°°°°°°°°°°°°°§")
-        print(result)
+
         with example_txt.open() as destination:
             self.assertEqual(destination.read(), result)
-

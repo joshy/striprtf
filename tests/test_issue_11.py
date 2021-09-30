a760b42589bfc945e0f14abd08f557c26b26e52a
Joshy Cyriac <joshy@posteo.ch>
2021-09-30 14:11:24 +0200

    fixed name of files

--- tests/test_issue_11.py
+++ tests/test_issue_11.py
@@ -9,8 +9,8 @@ TEXT_DIR = Path.cwd() / "tests" / "text"
 
 class TestUnicodeJapanese(unittest.TestCase):
     def test_empty(self):
-        example_rtf = RTF_DIR / "case_11.rtf"
-        example_txt = TEXT_DIR / "case_11.txt"
+        example_rtf = RTF_DIR / "issue_11.rtf"
+        example_txt = TEXT_DIR / "issue_11.txt"
 
         with example_rtf.open() as source:
             result = rtf_to_text(source.read())
